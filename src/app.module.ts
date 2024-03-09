a2be93c0ea1851c6f58488e38004eb35625b40b4
Sergey <Sr233@yandex.ru>
2024-03-09 14:17:43 +0300

    add empty User

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [DatabaseModule],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
